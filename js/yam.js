6776f40eaec4e19c7bf0486ff6ac22cffbc3ba89
ravi <55117331+ravidosa@users.noreply.github.com>
2025-07-07 08:04:35 -0700

    viz scraper

--- js/yam.js
+++ js/yam.js
@@ -1,26 +1,43 @@
+function slugify(text) {
+    let repl = text
+    .toString()
+    .normalize('NFD') // Normalize Unicode characters to their decomposed form
+    .replace(/[\u0300-\u036f]/g, '') // Remove diacritics (accents)
+    .toLowerCase() // Convert to lowercase
+    .trim() // Trim leading/trailing whitespace
+    .replace(/[^a-z0-9 -]/g, '') // Replace non-alphanumeric characters with empty string
+    .replace(/\s+/g, '-') // Replace spaces with hyphens
+    .replace(/-+/g, '-'); // Replace multiple hyphens with a single hyphen
+    return repl
+}
+
 class Root extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             categories: this.props.data.categories,
             series: this.props.data.series,
-            selected: null
+            selected: null,
+            upcoming: []
         };
         this.update = this.update.bind(this);
         this.delete = this.delete.bind(this);
         this.download = this.download.bind(this);
         this.upload = this.upload.bind(this);
+        this.getUpcoming = this.getUpcoming.bind(this);
+        this.getUpcoming();
     }
     update() {
         const form = document.getElementById("form");
         const formData = new FormData(form);
+        console.log(formData);
         const updated = {
             title: {en: formData.get("title-en"), int: formData.get("title-int")},
-            author: formData.get("author"),
-            artist: formData.get("artist"),
-            translator: formData.get("translator"),
+            staff: {author: formData.get("author"), artist: formData.get("artist"), translator: formData.get("translator")},
             volumes: formData.get("volumes"),
-            tags: formData.get("tags").split(/,\s*/)
+            publisher: formData.get("publisher"),
+            tags: formData.get("tags").split(/,\s*/).filter((tag) => tag.length > 0),
+            read: {volumes: formData.get("read-vol"), chapters: formData.get("read-chap")}
         };
         if (this.state.selected == -1) {
             this.setState({
@@ -68,15 +85,54 @@ class Root extends React.Component {
             reader.readAsText(file);
         }
     }
+    async getUpcoming() {
+        let upcoming = [];
+        const reading = this.state.series.filter((manga) => manga.tags.includes("reading"))
+        for (const manga of reading) {
+            if (manga.publisher == "VIZ Media") {
+                let response = await fetch(`https://api.allorigins.win/get?url=https%3A%2F%2Fwww.viz.com%2Fmanga-books%2Fmanga%2F${slugify(manga.title.en)}%2Fall`);
+                let json = await response.json();
+                let html = json.contents;
+                let doc = this.parseHTML(html);
+                const volumes = doc.querySelectorAll('.shelf article div a[href]');
+                if (volumes.length > manga.read.volumes) {
+                    const vol = volumes[manga.read.volumes];
+                    let response = await fetch(`https://api.allorigins.win/get?url=https%3A%2F%2Fwww.viz.com${vol.pathname}`);
+                    json = await response.json();
+                    html = json.contents;
+                    doc = this.parseHTML(html);
+                    let release = doc.querySelector('.o_release-date').innerHTML;
+                    release = release.slice(release.indexOf("</strong>") + 9).trim();
+                    upcoming.push({title: manga.title.en, volume: parseInt(manga.read.volumes) + 1, release: release});
+                }
+            }
+        }
+        this.setState({upcoming: upcoming})
+    }
+    parseHTML(htmlString) {
+        const parser = new DOMParser();
+        return parser.parseFromString(htmlString, 'text/html');
+    }
     render() {
         return(
             <div>
                 <div id="list">
-                    <div id="new-series" className="series" onClick={() => {this.setState({selected: -1})}}><h6>+ New Series</h6></div>
-                    <div id="edit-categories" className="series" onClick={() => {let categories = prompt("Edit Categories:", this.state.categories.join(", ")); this.setState({categories: categories.split(/,\s*/)})}}><h6>Edit Categories</h6></div>
+                    <div id="new-series" className="series" onClick={() => {this.setState({selected: -1})}}><h6>+ new series</h6></div>
+                    <div id="edit-categories" className="series" onClick={() => {let categories = prompt("Edit Categories:", this.state.categories.join(", ")); this.setState({categories: categories.split(/,\s*/).filter((category) => category.length > 0)}, () => localStorage.setItem("data", JSON.stringify(this.state)))}}><h6>edit categories</h6></div>
                     <div>
                         <input id="upload" type="file" onChange={this.upload}></input>
                         <button id="download" onClick={this.download}>Download</button>
+                    </div>
+                    <hr></hr>
+                    <div>
+                        upcoming volumes (tag "reading" to track)
+                        <ul>
+                            {this.state.upcoming.map((manga, index) => {
+                                return (
+                                    <li>{manga.title} {manga.volume} - {manga.release}</li>
+                                )
+                            })}
+                        </ul>
                     </div>
                     <hr></hr>
                     {
@@ -108,21 +164,40 @@ class Root extends React.Component {
                     <form id="form"  key={this.state.selected}>
                         <label for="select-title-en">Title (EN)</label>
                         <input id="select-title-en" name="title-en" placeholder="Title (EN)" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].title.en : ""}></input>
+
                         <label for="select-title-int">Title (INT)</label>
                         <input id="select-title-int" name="title-int" placeholder="Title (INT)" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].title.int : ""}></input>
+
                         <hr></hr>
                         <label for="select-author">Author</label>
-                        <input id="select-author" name="author" placeholder="Author" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].author : ""}></input>
+                        <input id="select-author" name="author" placeholder="Author" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].staff.author : ""}></input>
+
                         <label for="select-artist">Artist</label>
-                        <input id="select-artist" name="artist" placeholder="Artist" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].artist : ""}></input>
+                        <input id="select-artist" name="artist" placeholder="Artist" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].staff.artist : ""}></input>
+
                         <label for="select-translator">Translator</label>
-                        <input id="select-translator" name="translator" placeholder="Translator" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].translator : ""}></input>
+                        <input id="select-translator" name="translator" placeholder="Translator" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].staff.translator : ""}></input>
+
                         <hr></hr>
                         <label for="select-volumes">Volumes</label>
                         <input id="select-volumes" name="volumes" placeholder="0" type="number" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].volumes : ""}></input>
+
+                        <label for="select-publisher">Publisher</label>
+                        <select id="select-publisher" name="publisher">
+                            <option value="">select</option>
+                            <option value="VIZ Media" selected={this.state.selected != -1 ? this.state.series[this.state.selected].publisher == "VIZ Media" : false}>VIZ Media</option>
+                        </select>
+
                         <hr></hr>
+                        <label for="select-read-vol">Volumes Read</label>
+                        <input id="select-read-vol" name="read-vol" placeholder="0" type="number" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].read.volumes : ""}></input>
+
+                        <label for="select-read-chap">Chapters Read</label>
+                        <input id="select-read-chap" name="read-chap" placeholder="0" type="number" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].read.chapters : ""}></input>
+
                         <label for="select-tags">Tags</label>
                         <input id="select-tags" name="tags" placeholder="Tags" type="text" defaultValue={this.state.selected != -1 ? this.state.series[this.state.selected].tags.join(", ") : ""}></input>
+
                         <hr></hr>
                         <button disabled="" type="button" onClick={this.update}>{this.state.selected != -1 ? "Update" : "Add"}</button>
                         {this.state.selected != null && this.state.selected != -1 && 
